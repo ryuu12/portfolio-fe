8d088f8aa8960746aef4a7e980f5b3717ee918b2
Rayhan Rusyd <antianobl@gmail.com>
2022-07-02 16:53:56 +0700

    change pfp

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
             <div className="row">
                 <div className="desc">
                     <h1>Hi, It's me 👋</h1>
-                    <p>I’m Rayhan Rusyd, a Front End web developer. Right now 0I’am a student in Faculty of Industrial Engineering at Bandung Institute of Technology since 2022. I also love to work on some of my personal project to test my skills.</p>
+                    <p>I’m Rayhan Rusyd, a Front End web developer. Right now I’am a student in Faculty of Industrial Engineering at Bandung Institute of Technology since 2022. I also love to work on some of my personal project to test my skills.</p>
                     <p>Some of my Front End skills are related to my experience on using ReactJS and Next.js, while sometimes also learning a slight of Django.Py as the back-end framework. Not only on web development, but I also love to work on a small python related project such as creating a Discord bot.</p> 
                 </div>
             </div>
